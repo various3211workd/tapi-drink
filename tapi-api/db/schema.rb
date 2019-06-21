0fb8a3fc39ae6b38158f59fd35b9a7a493dfe1aa
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2019-06-21 19:20:43 +0900

    [add]password column

--- tapi-api/db/schema.rb
+++ tapi-api/db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_21_085426) do
+ActiveRecord::Schema.define(version: 2019_06_21_101048) do
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
+    t.string "password_digest", null: false
     t.string "user_token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
