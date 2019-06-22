721751b34a10a525e523b11bb78541c3477df276
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2019-06-22 15:52:55 +0900

    [change]routing

--- tapi-api/config/routes.rb
+++ tapi-api/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
-    post 'signup' , to: 'users#create'
+    post 'user/new' , to: 'users#create'
     post 'auth/login' , to: 'users#login'
   end
 end
