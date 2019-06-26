c0eda10ba3a1a2cd9d0a7b6786a737c9ce7d4ae8
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2019-06-26 13:44:06 +0900

    [add]setting foreign key & shop seed

--- tapi-api/app/models/shop.rb
+++ tapi-api/app/models/shop.rb
@@ -1,2 +1,3 @@
 class Shop < ApplicationRecord
+  belongs_to :user
 end
--- tapi-api/db/seeds.rb
+++ tapi-api/db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# coding: utf-8
+[
+  [
+    "タピオカジュース本店",
+    "XX県XX市XX区XX町XX丁目XX番地",
+    "タピオカを一から作成しておりもっちりとした触感がおいしいです",
+    1
+  ],
+  [
+    "新宿タピジュー",
+    "OO県OO市OO区OO町OO丁目OO番地",
+    "自分好きなようにドリンクを作成できるのでインスタ映えに最高です",
+    2
+  ],
+  [
+    "タピタピ",
+    "XX県XX市XX区XX町XX丁目XX番地",
+    "タピタピしてら",
+    3
+  ],
+  [
+    "tapi-drink",
+    "OO県OO市OO区OO町OO丁目OO番地",
+    "安い",
+    4
+  ]
+].each do |name, address, details, user_id|
+  User.transaction do
+    Shop.create!(
+      { 
+        name:     name,
+        address:  address,
+        details:  details,
+        user_id:  user_id
+      }
+    )
+    p "success create  { name: #{name}, address: #{address}, details: #{details}, user_id: #{user_id} }"
+  end
+  rescue => e
+    p "failed create { name: #{name}, address: #{address}, details: #{details}, user_id: #{user_id} }"
+end
