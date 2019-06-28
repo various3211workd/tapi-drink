d2e36eb7685fb6e7ffa2283ad8665517f2ac662c
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2019-06-28 11:30:50 +0900

    [wip]show shops list

--- tapi-api/app/controllers/api/shops_controller.rb
+++ tapi-api/app/controllers/api/shops_controller.rb
@@ -1,4 +1,9 @@
-class ShopsController < ApplicationController
-  def show
+module Api
+  class ShopsController < ApplicationController
+    skip_before_action :user_authenticate , only: [:show]
+
+    def show
+      shop_list = Shop.where(created_at:)
+    end
   end
 end
