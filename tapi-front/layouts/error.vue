0b8d9acf95da0051f25b434b5f14798c5df2e7ed
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2019-07-08 02:01:47 +0900

    [Add] create error.vue page

---
+++ tapi-front/layouts/error.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <h1 v-if="error.statusCode === 404">404 not found</h1>
+    <h1 v-else>error</h1>
+    <nuxt-link to="/">Back Top</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error']
+}
+</script>
+
+<style scoped>
+</style>
