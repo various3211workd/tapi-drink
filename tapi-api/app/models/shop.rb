7ca3f5ba0369a14815a5222b2d5b60a19ce31695
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2019-07-26 12:15:20 +0900

    [add]return images url

--- tapi-api/app/models/shop.rb
+++ tapi-api/app/models/shop.rb
@@ -9,39 +9,14 @@ class Shop < ApplicationRecord
   def self.fetch_shop_list(search_words)
     if search_words.blank?
       #fetch 10 latest record
-      shop_list = Shop.order('created_at desc').limit(10)
+      Shop.order('created_at desc').limit(10)
     else
       #fetch shop list
-      shop_list = shops_search_with(search_words)
-      p shop_list
+      shops_search_with(search_words)
     end
-
-    shop_list_json = []
-
-    shop_list.each do |item|
-      shop_list_json << {
-        name: item.name,
-        address: item.address,
-        details: item.details,
-        user_id: item.user_id,
-        user_name: item.user_name,
-        images_url: fetch_images_url_list(item)
-      }
-    end
-
-    shop_list_json
   end
 
   private
-
-  def self.fetch_images_url_list(shop_list)
-    images_url_list = []
-    unless shop_list.images.attached?
-      images_url_list << "/img/noimage.png"
-    end
-    
-    images_url_list
-  end
 
   def self.shops_search_with(search_words)
     shop_list = []
